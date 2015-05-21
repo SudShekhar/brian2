3f9dd9e31df28e6801a95c03394894dc79c5d9f2
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-05-21 15:15:37 +0200

    Use a newer conda-build version and fix version numbers in `meta.yml`

--- dev/continuous-integration/move-conda-package.py
+++ dev/continuous-integration/move-conda-package.py
@@ -3,16 +3,14 @@ import os
 import yaml
 import glob
 import shutil
-from conda_build import config
+try:
+    from conda_build.config import config
+except ImportError:
+    # For older versions of conda-build
+    from conda_build import config
 
 with open(os.path.join(sys.argv[1], 'meta.yaml')) as f:
     name = yaml.load(f)['package']['name']
-
-if not os.path.exists(config.bldpkgs_dir):
-    print('Cannot find path: %s' % config.bldpkgs_dir)
-else:
-    print('Files in path: %r' % os.listdir(config.bldpkgs_dir))
-    print('(looking for {0}*.tar.bz2)'.format(name))
 
 binary_package_glob = os.path.join(config.bldpkgs_dir, '{0}*.tar.bz2'.format(name))
 binary_package = glob.glob(binary_package_glob)[0]
