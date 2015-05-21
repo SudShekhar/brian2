b90c79655afc0d8efa5a22d02ce893eaee551ae2
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-05-21 14:48:56 +0200

    Upload conda packages to the `dev` channel

--- continuous-integration/binstar-push.py
+++ continuous-integration/binstar-push.py
@@ -4,7 +4,10 @@ import subprocess
 import traceback
 
 token = os.environ['BINSTAR_TOKEN']
-cmd = ['binstar', '-t', token, 'upload', '-u', 'brian-team', '--force']
+cmd = ['binstar', '-t', token, 'upload',
+       '-u', 'brian-team',
+       '--channel', 'dev',
+       '--force']
 cmd.extend(glob.glob('*.tar.bz2'))
 try:
     subprocess.check_call(cmd)
