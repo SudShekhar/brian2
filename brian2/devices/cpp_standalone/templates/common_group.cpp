d1d63ecc16b5ca9efea507db4e4073ac27181b62
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-05-28 17:38:53 +0200

    Support basic profiling in C++ standalone mode

--- brian2/devices/cpp_standalone/templates/common_group.cpp
+++ brian2/devices/cpp_standalone/templates/common_group.cpp
@@ -1,6 +1,7 @@
 {% macro cpp_file() %}
 #include "code_objects/{{codeobj_name}}.h"
 #include<cmath>
+#include<ctime>
 #include "brianlib/common_math.h"
 #include<stdint.h>
 #include<iostream>
@@ -19,6 +20,9 @@ namespace {
 void _run_{{codeobj_name}}()
 {	
 	using namespace brian;
+
+    const std::clock_t _start_time = std::clock();
+
 	///// CONSTANTS ///////////
 	%CONSTANTS%
 	///// POINTERS ////////////
@@ -39,6 +43,12 @@ void _run_{{codeobj_name}}()
 		{% endblock %}
 	}
 	{% endblock %}
+
+    {{ openmp_pragma('single') }}
+    {
+        const double _run_time = (double)(std::clock() -_start_time)/CLOCKS_PER_SEC;
+        {{codeobj_name}}_profiling_info += _run_time;
+    }
 }
 
 {% block extra_functions_cpp %}
